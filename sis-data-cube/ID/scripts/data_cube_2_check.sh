0cd8c6bcb5b387b7d7d6cac77f2f857b82eef731
eloi=ribeiro <mail@eloiribeiro.eu>
2025-08-22 17:23:56 +0200

    .

---
+++ sis-data-cube/ID/scripts/data_cube_2_check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+###############################################
+#                NoData & EPSG                #
+###############################################
+
+# Variables
+COUNTRY=ID
+INPUT_DIR="/home/carva014/Work/Code/FAO/GloSIS-dev/sis-data-cube/$COUNTRY/tmp"
+cd $INPUT_DIR
+
+# Chcek NoData values before processing
+printf "%-8s %-8s %-12s %-12s %-8s %s\n" "Minimum" "Maximum" "Mean" "StdDev" "NoData" "File"
+for FILE in *.tif; do
+    BASENAME=$(basename "$FILE")
+
+    # Extract NoData and stat values
+    CURRENT_NODATA=$(gdalinfo "$FILE" | grep "NoData Value=" | awk -F'NoData Value=' '{print $2}')
+    MIN=$(gdalinfo -stats "$FILE" | grep "Minimum=" | awk -F'Minimum=' '{print $2}' | awk -F',' '{print $1}')
+    MAX=$(gdalinfo -stats "$FILE" | grep "Minimum=" | awk -F'Maximum=' '{print $2}' | awk -F',' '{print $1}')
+    MEA=$(gdalinfo -stats "$FILE" | grep "Minimum=" | awk -F'Mean=' '{print $2}' | awk -F',' '{print $1}')
+    STD=$(gdalinfo -stats "$FILE" | grep "Minimum=" | awk -F'StdDev=' '{print $2}' | awk -F',' '{print $1}')
+    printf "%-8s %-8s %-12s %-12s %-8s %s\n" "$MIN" "$MAX" "$MEA" "$STD" "$CURRENT_NODATA" "$BASENAME"
+done
+
+rm *.tif.aux.xml
